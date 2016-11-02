8db31e0bd1023c99aa139eb8fc701ad481740b7a
Eric Dvorsak <eric.dvorsak@lambdawerk.com>
2016-11-02 18:20:23 +0100

    refactor

--- src/leiningen/validate.clj
+++ src/leiningen/validate.clj
@@ -1,6 +1,28 @@
 (ns leiningen.validate
   (:require [clojure.xml :as xml]))
 
+(defn deep-merge
+  ([] nil)
+  ([m] m)
+  ([m1 m2]
+   (reduce-kv (fn [out k v]
+                (let [v1 (get out k)]
+                  (cond (nil? v1)
+                        (assoc out k v)
+
+                        (and (map? v) (map? v1))
+                        (assoc out k (deep-merge v1 v))
+
+                        (= v v1)
+                        out
+
+                        :else
+                        (assoc out k v))))
+              m1
+              m2))
+  ([m1 m2 & ms]
+   (apply deep-merge (deep-merge m1 m2) ms)))
+
 (defn dependencies?
   [element]
   (= (:tag element) :dependencies))
@@ -14,27 +36,40 @@
                                                            dependency))]
     [(str groupId "/" artifactId) version]))
 
-(defn pom-file [project version]
-  (let [[groupId artifactId] (clojure.string/split (str project) #"/")
+(defn pom-file [project-full-name version]
+  (let [[groupId artifactId] (clojure.string/split project-full-name #"/")
         groupId (clojure.string/replace groupId #"\." "/")]
     (str (System/getProperty "user.home") "/.m2/repository/" groupId "/" artifactId "/" version "/"
          artifactId "-" version ".pom")))
 
-(defn get-dependencies
-  [project version]
-  (->> (pom-file project version)
-       xml/parse
-       :content
-       (filter dependencies?)
-       first
-       :content
-       (map :content)
-       (map clean-dependency)))
+(defn fetch-dependencies-from-pom
+  [[project-full-name version]]
+  (try
+    (->> (pom-file project-full-name version)
+         xml/parse
+         :content
+         (filter dependencies?)
+         first
+         :content
+         (map :content)
+         (mapv clean-dependency))
+    (catch java.net.MalformedURLException _
+      (println project-full-name version "could not be found, the pom file probably includes some variable version numbers")
+      nil)
+    (catch java.io.FileNotFoundException _
+      (println project-full-name version "could not be found, run lein deps or ignore this message")
+      nil)))
+
+(defn project-dependency-map
+  [project dependencies]
+  (zipmap dependencies
+          (map fetch-dependencies-from-pom dependencies)))
 
 (defn validate
   [project & args]
-  (-> (reduce (fn [acc [dependency version]]
-                (assoc acc dependency (get-dependencies dependency version)))
-              {}
-              (:dependencies project))
-      clojure.pprint/pprint))
+  (let [{:keys [name version dependencies]} project]
+    (-> (validate-project-dependencies [name version] project)
+        clojure.pprint/pprint)))
+
+#_(clojure.pprint/pprint (project-dependency-map ["lambdawerk/ess" "0.66.0-SNAPSHOT"]
+                                                 (fetch-dependencies-from-pom ["lambdawerk/ess" "0.66.0-SNAPSHOT"])))
