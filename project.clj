e8f7793bc20f39cfc6a5b65f3073eab5fdda61dc
Eric Dvorsak <eric@dvorsak.Fr>
2016-11-03 22:08:07 +0100

    version 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject validate "0.2.0"
+(defproject validate "0.2.1"
   :author "Eric Dvorsak"
   :description "A utility to validate clojure projects"
   :license {:name "Eclipse Public License"
