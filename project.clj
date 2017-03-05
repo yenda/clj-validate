064b752e0be93e1f9712a8356c886a5aba7f1ee0
Eric Dvorsak <eric@dvorsak.Fr>
2017-03-05 15:57:06 +0100

    add clojar deploy

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :description "A utility to validate clojure projects"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :deploy-repositories [["clojars" {:url "http://clojars.org/repo"
+                                    :sign-releases false}]])
